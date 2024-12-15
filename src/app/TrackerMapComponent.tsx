cd74b9ddcffe2af6ff0aa3e31bdcac61cbdbd83c
chaki8923 <aowp.contact@gmail.com>
2024-12-15 13:18:13 +0900

    実機デバッグ用4

--- src/app/TrackerMapComponent.tsx
+++ src/app/TrackerMapComponent.tsx
@@ -94,9 +94,11 @@ const TrackerMap = () => {
 
       const startTime = Date.now();
       timerRef.current = setInterval(() => {
-        const newTime = setElapsedTime(Math.floor((Date.now() - startTime) / 1000));
-        alert(`セット時間${newTime}`)
-        return newTime;
+        setElapsedTime((prevTime) => {
+          const newTime = Math.floor((Date.now() - startTime) / 1000);
+          alert(`セット時間: ${newTime}`); // newTimeを使用
+          return newTime;
+        });
       }, 1000);
 
       if (navigator.geolocation) {
