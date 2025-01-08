4bdcbfd7ad1fe824d86a9bc2908fe7b69f52133f
茶木 涼 <>
2025-01-08 14:59:54 +0900

    ログインできない検証 srcから出す戻し

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-// import TopSwiper from "./component/swiper/swiper";
-import Sample from "./sample";
+import TopSwiper from "./component/swiper/swiper";
+// import Sample from "./sample";
 
 export default async function Home() {
   return (
     <>
-      <Sample />
+      <TopSwiper />
     </>
   )
 } 
